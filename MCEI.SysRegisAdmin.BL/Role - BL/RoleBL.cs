65f05ab16ea72f1253b18f66a32340a92f7b1a78
Hector Garcia <hg425823@gmail.com>
2024-04-18 11:19:49 -0600

    Clase RoleBL con sus metodos necesarios

---
+++ MCEI.SysRegisAdmin.BL/Role - BL/RoleBL.cs
@@ -0,0 +1,67 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysRegisAdmin.DAL.Role___DAL;
+using MCEI.SysRegisAdmin.EN.Role___EN;
+
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.BL.Role___BL
+{
+    public class RoleBL
+    {
+        #region METODO PARA GUARDAR
+        // Metodo Para Guardar Un Nuevo Registro a La Base De Datos
+        public async Task<int> CreateAsync(Role role)
+        {
+            return await RoleDAL.CreateAsync(role);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Modificar Un Registro Existente En La Base De Datos
+        public async Task<int> UpdateAsync(Role role)
+        {
+            return await RoleDAL.UpdateAsync(role);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(Role role)
+        {
+            return await RoleDAL.DeleteAsync(role);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR TODOS
+        // Metodo Para Listar y Mostrar Todos Los Resultados
+        public async Task<List<Role>> GetAllAsync()
+        {
+            return await RoleDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA OBTENER POR ID
+        // Metodo Para Obtener Un Registro Por Su Id
+        public async Task<Role> GetByIdAsync(Role role)
+        {
+            return await RoleDAL.GetByIdAsync(role);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registro Existentes En La Base De Datos
+        public async Task<List<Role>> SearchAsync(Role role)
+        {
+            return await RoleDAL.SearchAsync(role);
+        }
+        #endregion
+    }
+}
