e711ae0b970126df3670d9050a345586da193e82
Hector Garcia <hg425823@gmail.com>
2024-04-18 10:30:10 -0600

    Entidades Role y User Creadas

---
+++ MCEI.SysRegisAdmin.EN/Role - EN/Role.cs
@@ -0,0 +1,35 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// References Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MCEI.SysRegisAdmin.EN.User___EN;
+
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.EN.Role___EN
+{
+    public class Role
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        [Display(Name = "Nombre")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ/ ]+$", ErrorMessage = "El Nombre debe contener solo Letras")]
+        public string Name { get; set; } = string.Empty;
+        #endregion
+
+        [NotMapped]
+        public int Top_Aux { get; set; } // propiedad auxiliar
+        public List<User>? Users { get; set; } // propiedad de navegación
+    }
+}
---
+++ MCEI.SysRegisAdmin.EN/User - EN/User.cs
@@ -0,0 +1,71 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using MCEI.SysRegisAdmin.EN.Role___EN;
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.EN.User___EN
+{
+    public class User
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Role")]
+        [Required(ErrorMessage = "El rol es requerido")]
+        [Display(Name = "Rol")]
+        public int IdRole { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [MaxLength(50, ErrorMessage = "Máximo 50 caracteres")]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Correo Electronico es requerido")]
+        [MaxLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        [Display(Name = "Correo Electronico")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "La contraseña debe estar entre 6 y 32 caracteres", MinimumLength = 6)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+
+        [Display(Name = "Fecha de registro")]
+        public DateTime RegistrationDate { get; set; }
+
+        #endregion
+
+        [NotMapped]
+        public int Top_Aux { get; set; } // propiedad auxiliar
+
+        [NotMapped]
+        [Required(ErrorMessage = "La confirmación de la contraseña es requerida")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        [StringLength(32, ErrorMessage = "La contraseña debe tener entre 6 y 32 caracteres", MinimumLength = 6)]
+        [Display(Name = "Confirmar la contraseña")]
+        public string ConfirmPassword_Aux { get; set; } = string.Empty; //propiedad auxiliar
+
+        public Role? Role { get; set; } //propiedad de navegación
+    }
+}
