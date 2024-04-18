7903e02eecdef887d2a80a4f1e46cf8a931c0138
Hector Garcia <hg425823@gmail.com>
2024-04-18 01:27:09 -0600

    Clase Para Conexion a La BD

---
+++ MCEI.SysRegisAdmin.DAL/ContextBD.cs
@@ -0,0 +1,29 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using Microsoft.EntityFrameworkCore;
+
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.DAL
+{
+    public class ContextBD : DbContext
+    {
+        #region REFERENCIAS DE TABLAS DE LA BD
+        //Coleccion que hace referencia a la tabla de la base de datos
+
+        #endregion
+
+        // Metodo de Conexion a la Base de Datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@""); // String de Conexion
+        }
+    }
+}
