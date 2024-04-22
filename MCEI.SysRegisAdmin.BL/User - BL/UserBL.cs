b0d6dcda4d1a5ac201c7bc970146d966381b6f38
Hector Garcia <hg425823@gmail.com>
2024-04-22 00:37:51 -0600

    Clase UserBL con sus metodos funcionales

---
+++ MCEI.SysRegisAdmin.BL/User - BL/UserBL.cs
@@ -0,0 +1,90 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysRegisAdmin.DAL.User___DAL;
+using MCEI.SysRegisAdmin.EN.User___EN;
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.BL.User___BL
+{
+    public class UserBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro a La Base De Datos
+        public async Task<int> CreateAsync(User user)
+        {
+            return await UserDAL.CreateAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Modificar Un Registro Existente En La Base De Datos
+        public async Task<int> UpdateAsync(User user)
+        {
+            return await UserDAL.UpdateAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(User user)
+        {
+            return await UserDAL.DeleteAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA OBTENER TODOS
+        // Metodo Para Listar y Mostrar Todos Los Resultados
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await UserDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA OBTENER POR ID
+        // Metodo Para Obtener Un Registro En Base a Su Id
+        public async Task<User> GetByIdAsync(User user)
+        {
+            return await UserDAL.GetByIdAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros En La Base De Datos
+        public async Task<List<User>> SearchAsync(User user)
+        {
+            return await UserDAL.SearchAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR INCLUYENGO LA LLAVE FORANEA
+        // Metodo Para Buscar Registros Incluyendo Las Llaves Foraneas
+        public async Task<List<User>> SearchIncludeRoleAsync(User user)
+        {
+            return await UserDAL.SearchIncludeRoleAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA INICIAR SESION (LOGIARSE)
+        // Metodo Para Autenticar El Usuario
+        public async Task<User> LoginAsync(User user)
+        {
+            return await UserDAL.LoginAsync(user);
+        }
+        #endregion
+
+        #region METODO PARA CAMBIAR CONTRASEÑA
+        // Metodo Para Cambiar La Contraseña Del Usuario
+        public async Task<int> ChangePasswordAsync(User user, string oldPassword)
+        {
+            return await UserDAL.ChangePasswordAsync(user, oldPassword);
+        }
+        #endregion
+    }
+}
