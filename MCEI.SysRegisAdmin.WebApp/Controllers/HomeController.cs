ffa4d5e033c492eef0781c7de3cf7ea8da90ccc8
Hector Garcia <hg425823@gmail.com>
2024-04-23 16:24:16 -0600

    Autentication  HomeController

--- MCEI.SysRegisAdmin.WebApp/Controllers/HomeController.cs
+++ MCEI.SysRegisAdmin.WebApp/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 using MCEI.SysRegisAdmin.WebApp.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace MCEI.SysRegisAdmin.WebApp.Controllers
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HomeController : Controller
     {
         public IActionResult Index()
