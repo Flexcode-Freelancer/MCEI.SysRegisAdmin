8c1c3cb88c5ea8df2510eee7274519a5342ec82a
Hector Garcia <hg425823@gmail.com>
2024-04-25 00:54:43 -0600

    Update Entidad ProfessionOrStudy

---
+++ MCEI.SysRegisAdmin.EN/ProfessionOrStudy - EN/ProfessionOrStudy.cs
@@ -0,0 +1,30 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations;
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.EN.ProfessionOrStudy___EN
+{
+    public class ProfessionOrStudy
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        [Display(Name = "Nombre")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ/ ]+$", ErrorMessage = "El Nombre debe contener solo Letras")]
+        public string Name { get; set; } = string.Empty;
+        #endregion
+
+        // public List<Membership> Memberships { get; set; } = new List<Membership>(); //Propiedad de navegacion
+    }
+}
