5c591d782689f170bb34f778a457b6f7cf7a8f81
Hector Garcia <hg425823@gmail.com>
2024-04-28 11:41:30 -0600

    First Config. Clase MembershipBL

---
+++ MCEI.SysRegisAdmin.BL/Membership - BL/MembershipBL.cs
@@ -0,0 +1,61 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funiconamiento
+using MCEI.SysRegisAdmin.DAL.Membership___DAL;
+using MCEI.SysRegisAdmin.EN.Membership___EN;
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.BL.Membership___BL
+{
+    public class MembershipBL
+    {
+        #region METODO PARA CREAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> CreateAsync(Membership membership)
+        {
+            return await MembershipDAL.CreateAsync(membership);
+        }
+        #endregion
+
+        
+
+
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<Membership>> GetAllAsync()
+        {
+            return await MembershipDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<Membership> GetByIdAsync(Membership membership)
+        {
+            return await MembershipDAL.GetByIdAsync(membership);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<Membership>> SearchAsync(Membership membership)
+        {
+            return await MembershipDAL.SearchAsync(membership);
+        }
+        #endregion
+
+        #region METODO PARA INCLUIR A PROFESION U OFICIO
+        public async Task<List<Membership>> SearchIncludeProfessionOrStudyAsync(Membership membership)
+        {
+            return await MembershipDAL.SearchIncludeProfessionOrStudyAsync(membership);
+        }
+        #endregion
+    }
+}
