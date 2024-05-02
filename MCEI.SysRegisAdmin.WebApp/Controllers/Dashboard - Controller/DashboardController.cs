21fca7f8c671f991de077d614605572785838f81
Hector Garcia <hg425823@gmail.com>
2024-05-02 11:16:23 -0600

    Create DashboardController

---
+++ MCEI.SysRegisAdmin.WebApp/Controllers/Dashboard - Controller/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.WebApp.Controllers.Dashboard___Controller
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles = "Desarrollador")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
