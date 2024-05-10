f42ee27aaa288e648066ca3f7b150a411e3cd186
Hector Garcia <hg425823@gmail.com>
2024-05-10 10:36:12 -0600

    Create EntityPrivilege

---
+++ MCEI.SysRegisAdmin.EN/Privilege - EN/Privilege.cs
@@ -0,0 +1,30 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations;
+
+
+#endregion
+
+namespace MCEI.SysRegisAdmin.EN.Privilege___EN
+{
+    public class Privilege
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        [Display(Name = "Nombre")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ/ ]+$", ErrorMessage = "El Nombre debe contener solo Letras")]
+        public string Name { get; set; } = string.Empty;
+        #endregion
+
+        //public List<Server> Servers { get; set; } = new List<Server>(); // Propiedad de navegacion
+    }
+}
